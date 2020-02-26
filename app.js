07dd2b4c728651ec4e3672a20ca71e8959ab3654
frehiwot <frehiwottm@gmail.com>
2020-02-25 19:45:49 -0600

    node practice

--- app.js
+++ app.js
@@ -2,3 +2,10 @@ const hellow = require("./index")
 //console.log(hellow.str)
 
 //console.log(hellow.person())
+
+//console.log(hellow.firstname + hellow.lastname)
+
+
+let cat = new hellow ("kity","white")
+
+console.log(cat.type())
--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 
 //module.exports.str = "hellow world "
-
+//using a function 
 exports.person = function(){
     console.log("hellow world")
 }
+
+//using object litral
+module.exports={
+   firstname :"frey",
+   lastname :"ye yeshi"
+}
+
+//using class
+ module.exports =function animal (name ,color){
+     this.name = name
+     this.color = color;
+     this.type=function(){
+        return this.name +this.color
+     }
+ }
